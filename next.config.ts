4d5f3f6e7e61ae91d969c582f8fdcd5a921c9721
Luis Toledo <luis_toledo35@hotmail.com>
2026-01-11 09:07:21 -0600

    gtag added

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: [
+      "default-src 'self'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
+      "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com",
+      "img-src 'self' data: blob: https://www.google-analytics.com https://www.googletagmanager.com https:",
+      "style-src 'self' 'unsafe-inline'",
+      "font-src 'self' data:",
+      "frame-src 'self' https://www.googletagmanager.com",
+    ].join("; "),
+  },
+  { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+];
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
@@ -19,7 +35,25 @@ const nextConfig: NextConfig = {
         hostname: "secure.gravatar.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "thagencia.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.thagencia.com",
+        pathname: "/**",
+      },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
