fcd8c8a2879b887bfca70f9a6b82c834a5fbe3a5
Luis Toledo <luis_toledo35@hotmail.com>
2026-01-10 21:16:50 -0600

    update
    update

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "thagencia.com",
+        hostname: "api.thagencia.com",
         pathname: "/**",
       },
       {
