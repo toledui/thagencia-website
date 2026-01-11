d0cc11119eab3cf7dd4dae92d678c8af5cecf33e
Luis Toledo <luis_toledo35@hotmail.com>
2026-01-11 11:56:57 -0600

    robots.txt fixed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/api/'],
+      },
+    ],
     sitemap: 'https://thagencia.com/sitemap.xml',
   };
 }
